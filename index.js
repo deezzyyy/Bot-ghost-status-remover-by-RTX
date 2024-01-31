b6575b84daa3c2e5297fc92e27526a1309e7dc7d
deezzyyy <157924645+deezzyyy@users.noreply.github.com>
2024-01-31 20:40:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["purchase gold today","nsfw","Sanctuary"];
 
 
 let currentIndex = 0;
